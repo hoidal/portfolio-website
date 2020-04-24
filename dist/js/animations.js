1b8e0c8c8909a64aacaaa10de088db67275e3612
hoidal <sjhoidal@gmail.com>
2020-04-24 17:10:57 -0600

    work added

--- dist/js/animations.js
+++ dist/js/animations.js
@@ -13,21 +13,46 @@ AOS.init();
 
 // Sticky and Current Nav Bar
 window.onscroll = () => updateNavBar();
-const navBar = document.querySelector(".main-nav");
-const topOfNav = navBar.offsetTop;
 
+// Nav Bar Dispatch Function
 const updateNavBar = () => {
     fixNav();
     showCurrentPage();
 }
  
-
+// Sticky Nav Bar
+const navBar = document.querySelector(".main-nav");
+const nextSection = document.querySelector("#home-a")
+const topOfNav = navBar.offsetTop;
 const fixNav = () => {
     if(window.pageYOffset >= topOfNav) {
-        navBar.classList.add("fixed-nav") 
+        navBar.classList.add("fixed-nav");
+        nextSection.style.paddingTop = "7rem";
     } else {
-        document.body.style.padding = 0;
         navBar.classList.remove("fixed-nav");
+        nextSection.style.paddingTop = "3rem";
+    }
+}
+
+// Nav Bar Current Page Highlighting
+// Nav List Items
+const navHome = document.querySelector("#nav-home");
+const navAbout = document.querySelector("#nav-about");
+const navWork = document.querySelector("#nav-work");
+const navPhotography = document.querySelector("#nav-photography");
+const navContact = document.querySelector("#nav-contact");
+
+//Sections
+const aboutMe = document.querySelector("#home-a");
+const aboutMeTop = aboutMe.offsetTop;
+
+const showCurrentPage = () => {
+    if(pageYOffset < aboutMeTop / 2) {
+        navAbout.className = "";
+        navHome.className = "current";
+    } else if(pageYOffset >= aboutMeTop / 2) {
+        navHome.className = "";
+        navAbout.className = "current";
     }
 }
 
